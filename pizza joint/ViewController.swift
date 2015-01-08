8e01ce27f2dabaffa4b96699ddc552a460e6be38
Adamzdogbossman <adamscottzillinger@CGTMAED-iMac-41.local>
2015-01-08 12:35:29 -0700

    Initial Commit

---
+++ pizza joint/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pizza joint
+//
+//  Created by Adam Scott Zillinger on 1/8/15.
+//  Copyright (c) 2015 Adam Scott Zillinger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
