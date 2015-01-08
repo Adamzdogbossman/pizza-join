90357786f025699d754ffd88174e01e0aedc87a4
Adamzdogbossman <adamscottzillinger@CGTMAED-iMac-41.local>
2015-01-08 13:42:55 -0700

    stuff

---
+++ pizza joint/pizza.swift
@@ -0,0 +1,25 @@
+//
+//  pizza.swift
+//  pizza joint
+//
+//  Created by Adam Scott Zillinger on 1/8/15.
+//  Copyright (c) 2015 Adam Scott Zillinger. All rights reserved.
+//
+
+import Foundation
+
+protocol PizzaOrdering{
+    func setSize(size:Sizes)
+    func setToppings(toppings:Array<Toppings>)
+    func Adress(adress:String)
+    func getTimeTillDeliverd()
+    func timeTillDeliverd()->Int
+    
+    
+}
+enum Sizes{
+    case Small,Medium,Larger
+}
+enum Toppings{
+    case Mushrooms,Cheese,Sasuge,Jalopenos
+}
